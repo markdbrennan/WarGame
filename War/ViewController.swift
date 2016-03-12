f24707744136df981d2be9667081e75d574eebb0
Mark Brennan <mbrennan456@icloud.com>
2016-03-12 17:09:41 +0000

    Initial Commit

---
+++ War/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Mark Brennan on 12/03/2016.
+//  Copyright © 2016 Mark Brennan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
